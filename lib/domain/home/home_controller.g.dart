bc5cd51d6df4a8406fef936103cb8f2b88b50c7f
portz <port.zhang@ehealth.com>
2026-08-29 21:01:55 +0800

    feat: add HomeController loading trends and recommendations in parallel

---
+++ lib/domain/home/home_controller.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(HomeController)
+final homeControllerProvider = HomeControllerProvider._();
+
+final class HomeControllerProvider
+    extends $AsyncNotifierProvider<HomeController, HomeData> {
+  HomeControllerProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'homeControllerProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$homeControllerHash();
+
+  @$internal
+  @override
+  HomeController create() => HomeController();
+}
+
+String _$homeControllerHash() => r'f11cc3c4c39a168aff020a93abfd2fe2c80ff210';
+
+abstract class _$HomeController extends $AsyncNotifier<HomeData> {
+  FutureOr<HomeData> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<AsyncValue<HomeData>, HomeData>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<HomeData>, HomeData>,
+              AsyncValue<HomeData>,
+              Object?,
+              Object?
+            >;
+    element.handleCreate(ref, build);
+  }
+}
